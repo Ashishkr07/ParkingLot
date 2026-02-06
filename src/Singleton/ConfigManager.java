72c10b11f3eff242087f5bc1efe7e500717bd6de
Ashish Kumar <ksndelash@gmail.com>
2026-02-06 12:46:04 +0530

    Singleton

---
+++ src/Singleton/ConfigManager.java
@@ -0,0 +1,19 @@
+package Singleton;
+
+public class ConfigManager {
+
+    private static ConfigManager instance;
+
+    private ConfigManager(){};
+
+    public static ConfigManager getInstance() {
+        if(instance == null){
+            synchronized(ConfigManager.class){
+                if(instance == null){
+                    instance = new ConfigManager();
+                }
+            }
+        }
+        return instance;
+    }
+}
